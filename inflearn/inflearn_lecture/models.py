93218fd4d4091ab9612f3269e4c3180359d6f6fd
peanutBana <alsdn@bagmin-uui-MacBookAir.local>
2022-07-11 10:00:12 +0900

    화면작성

--- inflearn/inflearn_lecture/models.py
+++ inflearn/inflearn_lecture/models.py
@@ -8,7 +8,10 @@ class myText(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     contents = models.CharField(max_length=200)
-    image_url = models.CharField(max_length=200)
+    img_url = models.FileField(null=True)
+
+    category = models.CharField(max_length=200, null=True)
+
     board_text = RichTextUploadingField(null=True)
 
     def publish(self):
